82f6d0811f64867a0825217da8ec9f624f88be1b
Michel Zehnder <MichelZ@users.noreply.github.com>
2018-10-29 11:08:24 +0100

    Update Application Insights

--- install-NugetPackages.ps1
+++ install-NugetPackages.ps1
@@ -1,7 +1,15 @@
-﻿cd .\PSAppInsights
+﻿#cd .\PSAppInsights
+
+if (!(Test-Path nuget.exe))
+{
+    $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+    $targetNugetExe = "nuget.exe"
+    Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+    Set-Alias nuget $targetNugetExe -Scope Global -Verbose
+}
 
 #Make sure to update Nuget 
 .\nuget.exe update -self
 #Now install the packages as specified in ther packages.config file 
-.\nuget.exe install .\packages.config -o .
+.\nuget.exe install .\packages.config -o ./packages
 
