bb63d9fd3d0decaf41517e48fc850d8c80746df2
Michel Zehnder <MichelZ@users.noreply.github.com>
2018-10-30 16:20:39 +0100

    build

--- build/build.ps1
+++ build/build.ps1
@@ -1,5 +1,6 @@
 Install-Module Psake, BuildHelpers, CredentialManager -force
 Install-Module Pester -Force -SkipPublisherCheck
 Import-Module Psake, BuildHelpers
+$PSScriptRoot\install-NugetPackages.ps1
 Set-BuildEnvironment
-Invoke-psake .\psakefile.ps1 -Verbose
+Invoke-psake $PSScriptRoot\psakefile.ps1 -Verbose
