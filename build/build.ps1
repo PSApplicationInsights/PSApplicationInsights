59bfd638a2920bb21909871bf3dba926abae9f89
Michel Zehnder <MichelZ@users.noreply.github.com>
2018-10-30 16:34:55 +0100

    build

--- build/build.ps1
+++ build/build.ps1
@@ -3,4 +3,4 @@ Install-Module Pester -Force -SkipPublisherCheck
 Import-Module Psake, BuildHelpers
 "$($PSScriptRoot)\install-NugetPackages.ps1"
 Set-BuildEnvironment
-Invoke-psake "$($PSScriptRoot)\psakefile.ps1" -Verbose
+Invoke-psake "$($PSScriptRoot)\psakefile.ps1" -Verbose Build
