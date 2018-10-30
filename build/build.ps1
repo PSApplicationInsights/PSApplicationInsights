b62cabda84d4ffc7e0281ba8286bad44cc5d3736
Michel Zehnder <MichelZ@users.noreply.github.com>
2018-10-30 15:37:27 +0100

    build

--- build/build.ps1
+++ build/build.ps1
@@ -1,2 +1,5 @@
-Import-Module psake
+Install-Module Psake, BuildHelpers, CredentialManager -force
+Install-Module Pester -Force -SkipPublisherCheck
+Import-Module Psake, BuildHelpers
+Set-BuildEnvironment
 Invoke-psake .\psakefile.ps1 -Verbose
